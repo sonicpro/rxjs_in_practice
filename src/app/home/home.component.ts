17730cdf2b0391a78d12e24aa51dc2ad0e5a3303
Volodymyr Tsurko <chegnenado@yahoo.com>
2021-06-26 13:39:51 +0300

    Split courses in next() implementation during subscribe() call.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,33 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Course } from '../model/course';
+import { CoursesObservableService } from '../services/courses-observable.service';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  beginnerCourses: Course[];
+  advancedCourses: Course[];
 
+  private subscription: Subscription;
 
-    constructor() {
-
-    }
+  constructor(private coursesObservableService: CoursesObservableService) {}
 
     ngOnInit() {
-
-
-
+      const coursesArray$ = this.coursesObservableService.getCoursesObservable();
+      this.subscription = coursesArray$.subscribe({
+        next: (courses: Course[]) => {
+          this.beginnerCourses = courses.filter(course => course.category === 'BEGINNER');
+          this.advancedCourses = courses.filter(course => course.category === 'ADVANCED');
+        },
+        complete: () => console.log('completed')
+      });
     }
 
+    ngOnDestroy() {
+      this.subscription.unsubscribe();
+    }
 }
