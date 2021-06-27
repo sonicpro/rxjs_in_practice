d7316d12299ba7ade4db67c12d1ab4e28d21b75b
Volodymyr Tsurko <chegnenado@yahoo.com>
2021-06-27 17:20:00 +0300

    Added saveButton functionality with throttling through exhaustMap().

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -2,10 +2,17 @@ import { AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild, OnDest
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Course } from '../model/course';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
-import { Observable, Subscription } from 'rxjs';
-import { concatMap, filter } from 'rxjs/operators';
+import { Observable, Subscription, fromEvent } from 'rxjs';
+import { concatMap, filter, exhaustMap } from 'rxjs/operators';
 import { fromPromise } from 'rxjs/internal-compatibility';
 import * as moment from 'moment';
+
+const responseObserver = {
+  next: (response: Response) => response.json()
+    .then(updatedCourse => console.log(updatedCourse))
+    .catch(err => console.log(err)),
+  error: (err: any) => console.log(err)
+};
 
 @Component({
   selector: 'app-course-dialog',
@@ -22,6 +29,7 @@ export class CourseDialogComponent implements OnInit, AfterViewInit, OnDestroy {
   course: Course;
 
   private subscription: Subscription;
+  private autoSaveSubscription: Subscription;
 
   constructor(
     private fb: FormBuilder,
@@ -40,26 +48,25 @@ export class CourseDialogComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   ngOnInit() {
-    const autosaveObservable = this.form.valueChanges.pipe(
-      filter(() => this.form.valid),
-      concatMap(formValue => this.saveCourse(this.course.id, formValue))
-    );
+    // const autosaveObservable = this.form.valueChanges.pipe(
+    //   filter(() => this.form.valid),
+    //   concatMap(formValue => this.saveCourse(this.course.id, formValue))
+    // );
 
-    this.subscription = autosaveObservable.subscribe({
-      next: (response: Response) => response.json()
-        .then(updatedCourse => console.log(updatedCourse))
-        .catch(err => console.log(err)),
-      error: (err: any) => console.log(err)
-    });
+    // this.autoSaveSubscription = autosaveObservable.subscribe(responseObserver);
   }
 
   ngAfterViewInit() {
-
-
+    const saveClickObservable$: Observable<MouseEvent> = fromEvent(this.saveButton.nativeElement, 'click');
+    const saveChangesObservable$ = saveClickObservable$.pipe(
+      exhaustMap(() => this.saveCourse(this.course.id, this.form.value))
+    );
+    this.subscription = saveChangesObservable$.subscribe(responseObserver);
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
+    this.autoSaveSubscription.unsubscribe();
   }
 
   close() {
